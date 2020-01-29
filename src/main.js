2975c8f2e5b79a0c62fad20e61026ba46124892d
iljah.p <i.poletajev@sauleit.com>
2020-01-29 17:44:33 +0200

    test

---
+++ src/main.js
@@ -0,0 +1,34 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Phaser.Game(config);
+var player;
+
+function preload ()
+{
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+
+}
+
+function update ()
+{
+}
